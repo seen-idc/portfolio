17adc6591400697f4e36ddafb80b5c715a0b9a27
seen-idc <seen-idc@github.com>
2021-11-11 20:42:43 +1100

    ok

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,184 +1,116 @@
 import * as React from 'react'
 import { CSSProperties } from 'react'
+import { Helmet } from 'react-helmet'
+import { SocialIcon } from 'react-social-icons'
 
-// styles
 const pageStyles: CSSProperties = {
-  color: '#232129',
-  padding: 96,
-  fontFamily: '-apple-system, Roboto, sans-serif, serif',
-}
-const headingStyles: CSSProperties = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles: CSSProperties = {
-  color: '#663399',
-}
-const paragraphStyles: CSSProperties = {
-  marginBottom: 48,
-}
-const codeStyles: CSSProperties = {
-  color: '#8A6534',
-  padding: 4,
-  backgroundColor: '#FFF4DB',
-  fontSize: '1.25rem',
-  borderRadius: 4,
-}
-const listStyles: CSSProperties = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles: CSSProperties = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
+  paddingTop: '5%',
+  paddingLeft: '15%',
+  color: 'white',
 }
 
-const linkStyle: CSSProperties = {
-  color: '#8954A8',
+const normalTextStyles: CSSProperties = {
+  fontSize: '1.5rem',
+  fontFamily: 'Nunito, san-serif',
+}
+
+const titleGlow = 'rgba(173, 211, 255, 0.3)'
+const titleStyles: CSSProperties = {
+  fontSize: '3rem',
+  color: '#add3ff',
+  fontFamily: 'Raleway, sans-serif',
   fontWeight: 'bold',
-  fontSize: 16,
-  verticalAlign: '5%',
+  textShadow: `0 0 7px ${titleGlow},
+  0 0 10px ${titleGlow},
+  0 0 21px ${titleGlow}`,
 }
 
-const docLinkStyle: CSSProperties = {
-  ...linkStyle,
+const valorantLinkStyles: CSSProperties = {
+  fontSize: '1.3rem',
+  fontFamily: 'Roboto Mono, monospace',
+  color: '#E2404F',
+  letterSpacing: '2px',
+  textDecoration: 'none',
+  borderRadius: '3px',
+  backgroundColor: 'rgba(255, 117, 130, 0.3)',
+  padding: '3px 3px',
+}
+
+const socialsListStyles: CSSProperties = {
+  display: 'inline',
   listStyleType: 'none',
-  marginBottom: 24,
+  margin: 0,
+  padding: 0,
 }
 
-const descriptionStyle: CSSProperties = {
-  color: '#232129',
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
+const socialsListItemStyle: CSSProperties = {
+  float: 'left',
+  marginLeft: '1rem',
 }
 
-const docLink = {
-  text: 'Documentation',
-  url: 'https://www.gatsbyjs.com/docs/',
-  color: '#8954A8',
-}
-
-const badgeStyle: CSSProperties = {
-  color: '#fff',
-  backgroundColor: '#088413',
-  border: '1px solid #088413',
-  fontSize: 11,
-  fontWeight: 'bold',
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: '4px 6px',
-  display: 'inline-block',
-  position: 'relative',
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-// data
-const links = [
-  {
-    text: 'Tutorial',
-    url: 'https://www.gatsbyjs.com/docs/tutorial/',
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: '#E95800',
-  },
-  {
-    text: 'How to Guides',
-    url: 'https://www.gatsbyjs.com/docs/how-to/',
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: '#1099A8',
-  },
-  {
-    text: 'Reference Guides',
-    url: 'https://www.gatsbyjs.com/docs/reference/',
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: '#BC027F',
-  },
-  {
-    text: 'Conceptual Guides',
-    url: 'https://www.gatsbyjs.com/docs/conceptual/',
-    description:
-      'Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.',
-    color: '#0D96F2',
-  },
-  {
-    text: 'Plugin Library',
-    url: 'https://www.gatsbyjs.com/plugins',
-    description:
-      'Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.',
-    color: '#8EB814',
-  },
-  {
-    text: 'Build and Host',
-    url: 'https://www.gatsbyjs.com/cloud',
-    badge: true,
-    description:
-      'Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!',
-    color: '#663399',
-  },
-]
-
-// markup
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{' '}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
+    <>
+      <Helmet>
+        <style>{'body { background-color: #0b141f;}'}</style>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&family=Raleway:wght@700&family=Roboto+Mono&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+      <main style={pageStyles}>
+        <h1 style={titleStyles}>seen</h1>
+        <p style={normalTextStyles}>
+          I play games like{' '}
           <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
+            style={valorantLinkStyles}
+            href="https://tracker.gg/valorant/profile/riot/seen%20idc%23pimi%E3%83%84/overview"
           >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map((link) => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
+            VALORANT
+          </a>{' '}
+          and like to delve into all things relating to computers like
+          programming.
+        </p>
+        <ul style={socialsListStyles}>
+          <li style={socialsListItemStyle}>
+            <SocialIcon
+              fgColor="white"
+              url="https://twitch.tv/seen_idc"
+            ></SocialIcon>
           </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+          <li style={socialsListItemStyle}>
+            <SocialIcon
+              fgColor="white"
+              url="https://www.youtube.com/channel/UCB_DJKfNs24ogACFh3EuzKg"
+            ></SocialIcon>
+          </li>
+          <li style={socialsListItemStyle}>
+            <SocialIcon
+              fgColor="white"
+              url="https://github.com/seen-idc"
+            ></SocialIcon>
+          </li>
+          <li style={socialsListItemStyle}>
+            <SocialIcon
+              fgColor="white"
+              url="https://twitter.com/seen_val"
+            ></SocialIcon>
+          </li>
+          <li style={socialsListItemStyle}>
+            <SocialIcon
+              fgColor="white"
+              url="https://discord.gg/dBbspZ2CqU"
+            ></SocialIcon>
+          </li>
+        </ul>
+      </main>
+    </>
   )
 }
 
